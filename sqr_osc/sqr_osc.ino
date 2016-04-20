b7808dd355c524d56d3ce3f523134c309482b925
Atte André Jensen <atte.jensen@gmail.com>
2016-04-20 14:45:43 +0200

    Added (optional) quantization to semitones

--- sqr_osc/sqr_osc.ino
+++ sqr_osc/sqr_osc.ino
@@ -1,10 +1,21 @@
 
 /*
-Square wave oscillator
 Alternate firmware for the ginky synthese grains eurorack module
 Code by a773 (atte.dk) and released under the GPL licence
 
+Square wave oscillator
+
+Upper knob/in 1:  quantize to semitones, CCW: quantize off, CW quantize on
+Middle knob/in 2: unused
+Lower knob:       tune (range 1 octave)
+In3;              1v/oct pitch input
+Out:              Audio out
+
 Calibration:
+IMPORTANT: unfortunately you have to power the module from your rack when 
+measuring, you can't rely on the tuning when powered from USB, remember to
+NOT power from USB and rack at the same time, since this might blow up your module!
+
 1) with the lowest pot turned all the way CCW, send 0V into input 3
 2) the pitch should be C2 (65Hz) 
 3) with 1V into input 3, adjust the value of VOCT_SCALE until the pitch
@@ -15,50 +26,71 @@ Calibration:
 5) optionally adjust BASE_OCTAVE to shift octaves up (3 is one octave higher)
 */
 
-//calibration
-#define VOCT_SCALE    1.0
-#define KNOB_SCALE    1.0
-#define BASE_OCTAVE   2 // base octave not lower than 2
+// calibration
+#define VOCT_SCALE         1.0
+#define KNOB_SCALE         1.0
+#define BASE_OCTAVE        2 // base octave not lower than 2
 
-#define PWM_PIN       11
-#define TUNE_KNOB      0
-#define TUNE_KNOB_RANGE 596
+// setup
+#define PWM_PIN            11
+#define VOCT               3
+#define TUNE_KNOB          0
+#define TUNE_KNOB_RANGE    1024
+#define UPPER_POT          2
+#define UPPER_KNOB_RANGE   1024
+#define OCT_RANGE          5
 
-#define VOCT          3
-#define NB_VOLTS 9
+
+// stuff used for smoothing
+#define NB_SMOOTHING       5
+float readings[NB_SMOOTHING];     // the readings from the analog input
+int readIndex = 0;                // the index of the current reading
+float total = 0;                  // the running total
+float average = 0;                // the average
 
 float freq;
-float volt, volt1, volt2, volt3, volt4, volt5, volt6, volt7, volt8;
+float volt;
 
-float volt2freq(float volt){
-  return 440 / pow(2, 4.75) * pow(2, min(volt,3) + BASE_OCTAVE); 
+float smooth(float value_to_smooth){
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = value_to_smooth;
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= NB_SMOOTHING) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / NB_SMOOTHING;
+
+  return average;
 }
 
+float volt2freq(float volt){
+  return 440 / pow(2, 4.75) * pow(2, min(volt,OCT_RANGE) + BASE_OCTAVE); 
+}
+
+float quantize(float volt){
+   return round(volt * 12) / 12.0; 
+}
 
 void setup() {
-  volt = (1.0 * analogRead(TUNE_KNOB)/TUNE_KNOB_RANGE) + (analogRead(VOCT)/220.2);
-  volt1 = volt;
-  volt2 = volt;
-  volt3 = volt;
-  volt4 = volt;
-  volt5 = volt;
-  volt6 = volt;
-  volt7 = volt;
-  volt8 = volt;
 }
 
 void loop() {
-  volt = (1.0 * analogRead(TUNE_KNOB)/TUNE_KNOB_RANGE*KNOB_SCALE) + (analogRead(VOCT)/220.2*VOCT_SCALE);
-  
-  freq = volt2freq((volt + volt1 + volt2 + volt3 + volt4 + volt5 + volt6 + volt7 + volt8)/NB_VOLTS);
+  volt = (1.0 * analogRead(TUNE_KNOB)/TUNE_KNOB_RANGE*KNOB_SCALE) + (analogRead(VOCT)/204.0*VOCT_SCALE);
+  volt = smooth(volt);
+  if(analogRead(UPPER_POT) > UPPER_KNOB_RANGE / 2){
+    volt = quantize(volt);
+  }
+  freq = volt2freq(volt);
   tone(PWM_PIN,freq);
-  volt8 = volt7;
-  volt7 = volt6;
-  volt6 = volt5;
-  volt5 = volt4;
-  volt4 = volt3;
-  volt3 = volt2;
-  volt2 = volt1;
-  volt1 = volt;
        
 }
